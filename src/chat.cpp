b27d5b962769478f7961a2e7fa2a9e9ff2bc02d8
Ian <lobpqq@gmail.com>
2022-10-11 12:36:55 -0600

    Add given source files

---
+++ src/chat.cpp
@@ -0,0 +1,20 @@
+/*******************************************
+* Group Name  : XXXXXX
+
+* Member1 Name: Ian Snyder
+* Member1 SIS ID: 831615373
+* Member1 Login ID: isnyder
+
+* Member2 Name: Daniel Ives
+* Member2 SIS ID: XXXXXX
+* Member2 Login ID: XXXXXX
+
+* Member2 Name: Lorenzo Garcia
+* Member2 SIS ID: XXXXXX
+* Member2 Login ID: XXXXXX
+********************************************/
+
+int main(int argc, char* argv[]){
+    
+    return 0;
+}
